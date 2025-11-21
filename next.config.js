911177cff14d57dfc0a6d0d2bdbb3da3012ec4c8
vamshi5654 <bandarivamshi07@gmail.com>
2025-11-21 22:00:31 +0530

    Fix healthz route and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   async rewrites() {
     return [
       {
-        source: "/healthz",
-        destination: "/api/healthz",
+        source: "/:code",
+        destination: "/code/:code",
       },
     ];
   },
