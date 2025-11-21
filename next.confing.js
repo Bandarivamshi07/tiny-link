16576a481bbbe8533acb4560ab3b5531a5e8b902
vamshi5654 <bandarivamshi07@gmail.com>
2025-11-21 21:36:08 +0530

    Fix: Correct healthz route + rewrite

--- next.confing.js
+++ next.confing.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/healthz",
+        destination: "/api/healthz",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
